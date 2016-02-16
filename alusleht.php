ce42480da9d8ddd6c75bbf9c5aa219d93ee6cbcc
polika1311 <ppopova@itcollege.ee>
2016-02-17 00:26:59 +0200

    kodutoo1

---
+++ alusleht.php
@@ -0,0 +1,44 @@
+﻿
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" />
+
+<title> Minu testimised </title>
+
+<body>
+<h1>Jupiter</h1>
+
+<br/>
+<p><b>Jupiter </b> on Päikesest kauguselt viies planeet ja Päikesesüsteemi kõige suurem planeet. Hiidplaneet Jupiter ületab kõigi teiste planeetide kogumassi umbes kaks ja pool korda. Päikese massist on Jupiteri mass ligikaudu 1000 korda väiksem. Astronoomid teavad Jupiteri juba antiikajast.[1] Roomlased panid sellele nime oma jumala Jupiteri järgi. Maa pealt vaadatuna võib Jupiteri tähesuurus olla kuni ?2,94, mis on piisavalt hele, et jätta varje.[2] Kuu ja Veenuse järel on Jupiter heleduselt kolmas taevakeha. (Opositsiooni ajal võib Marss paista lühikest aega sama hele kui Jupiter.)
+</p>
+<p><em> Jupiter </em> koosneb peamiselt heeliumist, mis moodustab Jupiteri massist ühe neljandiku, kõigist molekulidest moodustavad heeliumi molekulid aga vaid kümnendiku. Jupiteril võib olla raskematest elementidest tuum,[3] kuid nagu kõigil hiidplaneetidel, puudub ka Jupiteril tahke pind. Kiire pöörlemise tõttu on Jupiter lapik sferoid. Jupiteri välimine atmosfäär on silmanähtavalt jaotunud vastavalt laiuskraadidele erinevateks kihtideks, mille piirialadel esineb turbulentsi ja torme. Üks tähelepänuväärsemaid Jupiteri atmosfäärinähtusi on Suur Punane Laik, mis on olnud olemas juba vähemalt 17. sajandist, mil seda vaadeldi esmakordselt teleskoobiga. Jupiteri ümbritseb tugev magnetväli ja palja silmaga nähtamatu rõngaste süsteem. Jupiteril on vähemalt 67 kaaslast, sealhulgas neli suurt kaaslast, mille avastas 1610. aastal Galileo Galilei. Neist kõige suurema, Ganymedese läbimõõt on suurem kui Merkuuril.
+</p>
+<br/>
+<a href="http://www.w3schools.com/" target="_blank">Wikipeedia</a>
+<p>
+<img src="Jupiter.jpg" alt="Jupiter" width="250" height="250" />
+</p>
+<h2>Mass ja suurus</h2> 
+<br/>
+<p>
+Jupiteri mass on 2,5 korda suurem kui kõigi teiste Päikesesüsteemi planeetide kogumass. See tähendab, et Jupiteri 
+ja Päikese massikese asub väljaspool Päikest. Jupiteri kõrval on Maa imeväike, kuid märkimisväärselt suurema tihedusega.
+ Jupiteri ruumala on 1321 korda suurem kui Maa ruumala, kuid selle mass on vaid 318 korda suurem. Jupiteri raadius on
+  umbes kümnendik Päikese raadiusest[11] ja selle mass on umbes 0,001 Päikese massi, seega on Päike ja Jupiter sarnase 
+  tihedusega.
+<h3>Möödalennumissioonid</h3>
+<br/>
+<ul>
+	<li>Pioneer 10  3. detsember 1973   </li>
+	<li>Pioneer 11  4. detsember 1974   </li>
+	</ul>
+
+<pre>
+Jätkub</pre>
+<p>
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+ </a>
+</p>
+</body>
+</html>
