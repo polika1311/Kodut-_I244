0eea00839aeeb3afbe0c60af909cc75ced2852f9
polika1311 <ppopova@itcollege.ee>
2016-03-04 16:39:40 +0200

    Kodutöönr3

---
+++ Kodutoonr3/stiilid.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>3 kodune töö</title>
+	<style type="text/css">
+	p.green {
+		color:green;
+	}
+	p#un{ text-decoration: underline; }
+	p.darkgreen {background-color:green;
+}
+p#twice{
+	font-size:24px
+}
+p#font{
+	font-family:monospace;
+}
+p.yellow{
+	border-width:10px;	
+    border:solid black;
+    background-color:yellow;
+}
+p.grey{
+	border-width:10px;	
+    border:dashed black;
+    border-radius:15px;
+}
+p#white{
+	font-style:italic;
+	 text-align: center; 
+}
+p.crosshair
+{
+    cursor: crosshair;
+}
+
+    
+    
+	</style>
+</head>
+<body>
+
+<p class="green">minu sees olev tekst on roheline</p>
+
+<p id="un">minu sees olev teks on alla joonitud </p>
+<p class ="darkgreen">minul on tuhmroheline taust</p>
+<p id="twice">minul on teistest 2x suurem fondi suurus</p>
+<p id="font">minul on tekst monospace fondiga</p>
+<p class="yellow">minul on must paks piirjoon ning kollane taust</p>
+<p class="grey">minul on ümmargused nurgad ja katkendlik piirjoon</p>
+<p id="white">minu tekst onkaldkirjas ja keskele joondatud</p>
+<p class="crosshair">minu kohale minnes muudetakse kursor crosshair-iks</p>
+<!-- siia võite ise elemente juurde ka panna, et harjutada erinevaid CSS omadusi ja selektoreid -->
+
+
+<!-- JÄRGNEV EI OLE ÜLESANDEGA OTSESELT SEOTUD -->
+<hr/>
+<p>valideerimiseks: 
+ <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
+</p>
+</body>
+</html>
